e0652d1c48391570747da07469e2b9970648f195
kchalouah <Karim.chalouah2@gmail.com>
2026-01-10 13:55:40 +0100

    backend 95%

--- Backend/mail-service/src/main/java/com/medinsight/mail/controller/MailController.java
+++ Backend/mail-service/src/main/java/com/medinsight/mail/controller/MailController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/mail")
+@RequestMapping("/mail")
 @RequiredArgsConstructor
 @Tag(name = "Email Service", description = "Endpoints for sending system notifications and reminders")
 public class MailController {
